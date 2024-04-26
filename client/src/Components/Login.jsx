9571c9f1ea989f37e5c47c4fcd3a1f61e99a354c
PJPhukan <paragjyotiphukan2004@gmail.com>
2024-04-26 23:34:22 +0530

    Login section completed

--- client/src/Components/Login.jsx
+++ client/src/Components/Login.jsx
@@ -1,11 +1,54 @@
-import React from 'react'
-
+import React from "react";
+import { Link } from "react-router-dom";
+import Google from "../assets/google.png"
 const Login = () => {
   return (
-    <div>
-      This is Login page
-    </div>
-  )
-}
+    <section className="login">
+      <div className="container">
+        <h1 className="heading">Welcome Back!</h1>
+        <p className="text">
+          Enter your details below to sign in your account.
+        </p>
 
-export default Login
+        <form>
+          <div className="input-field email">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Enter your email"
+            />
+          </div>
+          <div className="input-field password">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Enter your password"
+            />
+          </div>
+          <Link to="/forgot-password" className="forgotPassword">
+            Forgot Password?
+          </Link>
+          <button className="submit" type="submit">
+            Login
+          </button>
+        </form>
+
+        <div className="or">
+          <span className="line">or</span>
+        </div>
+        <button className="login-with">
+          <Link to="/">
+            <img src={Google} alt="" />
+            <span> Continue with Google</span>
+          </Link>
+        </button>
+      </div>
+    </section>
+  );
+};
+
+export default Login;
